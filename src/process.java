bed14fb85b3f56087ce5aa84f41484f539df9f97
chunkitchung <38967163+chunkitchung@users.noreply.github.com>
2021-03-20 23:31:52 -0400

    Update process.java

--- src/process.java
+++ src/process.java
@@ -1,38 +1,66 @@
-
-public class process {
+public class process implements java.lang.Comparable<process> {
 	int arrivalTime;
-	int burstTime; //Temporary to simplify schedulling
+	int burstTime; // Temporary to simplify schedulling
 	// I/O requirements
 	int processPriority;
-	
+	String processNum;
+	int numOfProcess;
+
 	public process(int aTime, int bTime) {
 		this.arrivalTime = aTime;
 		this.burstTime = bTime;
 	}
-	
+
 	public process(int aTime, int bTime, int priority) {
 		this.arrivalTime = aTime;
 		this.burstTime = bTime;
 		this.processPriority = priority;
 	}
-	
+
+	public process(String processNum, int numOfProcess, int burstTime, int processPriority) {
+		this.processNum = processNum;
+		this.numOfProcess = numOfProcess;
+		this.burstTime = burstTime;
+		this.processPriority = processPriority;
+	}
+
+	public process(String processNum, int burstTime, int processPriority) {
+		this.processNum = processNum;
+		this.burstTime = burstTime;
+		this.processPriority = processPriority;
+	}
+
+	public int getNumOfProcess() {
+		return numOfProcess;
+	}
+
 	public int getArrivalTime() {
 		return arrivalTime;
 	}
+
 	public void setArrivalTime(int arrivalTime) {
 		this.arrivalTime = arrivalTime;
 	}
+
 	public int getBurstTime() {
 		return burstTime;
 	}
+
 	public void setBurstTime(int burstTime) {
 		this.burstTime = burstTime;
 	}
+
 	public int getProcessPriority() {
 		return processPriority;
 	}
+
 	public void setProcessPriority(int processPriority) {
 		this.processPriority = processPriority;
 	}
-	
+
+	@Override
+	public int compareTo(process compareProcess) {
+		int comparePriority = ((process) compareProcess).getProcessPriority();
+		return this.processPriority - comparePriority;
+	}
 }
